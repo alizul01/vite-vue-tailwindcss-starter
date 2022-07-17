ca6ca72396a2b929df0aa4385d7e3dea194124e7
Ali Zulfikar <alizulfikar032@gmail.com>
2022-07-17 07:43:49 +0700

    Initialize Repository

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <nav class="p-2 flex justify-center items-center">
+      <router-link class="mr-2" to="/">Home</router-link>
+      <router-link class="mr-2" to="/about">About</router-link>
+    </nav>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <div class="flex justify-center items-center flex-col mt-[20rem]">
+        <h1 class="font-bold text-4xl">{{ message }}</h1>
+
+        <p class="font-normal mt-2 text-lg">
+            {{ description }}
+        </p>
+        <div class="grid grid-cols-3 gap-8 mt-12">
+            <div id="tailwindcss">
+                <img class="w-32" src="https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png" alt="Tailwind CSS" />
+            </div>
+            <div id="vue">
+                <img class="w-24" src="https://vuejs.org/images/logo.png" alt="Vue.js" />
+            </div>
+            <div id="volar">
+                <img class="w-24" src="https://camo.githubusercontent.com/61e102d7c605ff91efedb9d7e47c1c4a07cef59d3e1da202fd74f4772122ca4e/68747470733a2f2f766974656a732e6465762f6c6f676f2e737667" alt="Lite" />
+            </div>
+        </div>
+
+        <button class="p-4 bg-slate-700 text-white mt-4 rounded-lg" type="button" @click="count++">count is {{ count }}</button>
+
+        <!-- change message by input then clicked the button -->
+        <label class="mt-4" for="message">Change title :</label>
+        <input type="text" v-model="message" class="border border-slate-700 p-2 rounded-md"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                message: 'Welcome',
+                description: 'This is Vue.js project use Tailwind CSS and Vite'
+            }
+        }
+    }
+</script>
